bdb41390940ffec55f7725c6104c8a8d6bac3a48
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2021-12-01 21:14:11 -0800

    Day 2

---
+++ 2021/Day2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace Day2 {
+    // https://adventofcode.com/
+    public static class Program {
+        public static void Main() {
+            string file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+            string[] input = File.ReadAllLines(file);
+            
+            Console.WriteLine(Part1(input));
+            Console.WriteLine(Part2(input));
+        }
+
+        private static int Part1(string[] input) {
+            int x = 0;
+            int y = 0;
+            foreach (string row in input) {
+                string[] e = row.Split(" ");
+                switch (e[0]) {
+                    case "forward":
+                        x += int.Parse(e[1]);
+                        break;
+                    case "down":
+                        y += int.Parse(e[1]);
+                        break;
+                    case "up":
+                        y -= int.Parse(e[1]);
+                        break;
+                }
+            }
+            return x * y;
+        }
+        
+        private static int Part2(string[] input) {
+            int x = 0;
+            int y = 0;
+            long aim = 0;
+            foreach (string row in input) {
+                string[] e = row.Split(" ");
+                switch (e[0]) {
+                    case "forward":
+                        x += int.Parse(e[1]);
+                        y += (int)aim * int.Parse(e[1]);
+                        break;
+                    case "down":
+                        aim += int.Parse(e[1]);
+                        break;
+                    case "up":
+                        aim -= int.Parse(e[1]);
+                        break;
+                }
+            }
+
+            return x * y;
+        }
+    }
+}
