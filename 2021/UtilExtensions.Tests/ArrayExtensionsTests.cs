fc767157d896a22325596ba00165e3d771097e9a
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2021-12-08 23:31:07 -0800

    Add ArrayExtensions

---
+++ 2021/UtilExtensions.Tests/ArrayExtensionsTests.cs
@@ -0,0 +1,74 @@
+using System;
+using NUnit.Framework;
+
+namespace UtilExtensions.Tests {
+    public class ArrayExtensionsTests {
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public void TryGetTest() {
+            int[,] array = new int[2, 4];
+            array[0, 0] = 10;
+            array[1, 2] = 20;
+            
+            Assert.True(array.TryGet(0, 0, out int coord1));
+            Assert.AreEqual(10, coord1);
+            Assert.True(array.TryGet(1, 2, out int coord2));
+            Assert.AreEqual(20, coord2);
+            Assert.True(array.TryGet(1, 1, out int coord3));
+            Assert.AreEqual(0, coord3);
+            
+            // Out of bounds
+            Assert.False(array.TryGet(-1, 0, out int _));
+            Assert.False(array.TryGet(0, -1, out int _));
+            Assert.False(array.TryGet(2, 0, out int _));
+            Assert.False(array.TryGet(0, 4, out int _));
+        }
+
+        [Test]
+        public void GetColumnTest() {
+            int[,] array = new int[2, 4];
+            array[0, 0] = 10;
+
+            Assert.AreEqual(new []{10, 0}, array.GetColumn(0));
+            Assert.AreEqual(new []{0, 0}, array.GetColumn(1));
+            Assert.Throws<IndexOutOfRangeException>(() => array.GetColumn(-1));
+            Assert.Throws<IndexOutOfRangeException>(() => array.GetColumn(4));
+        }
+
+        [Test]
+        public void GetRowTest() {
+            int[,] array = new int[2, 4];
+            array[0, 0] = 10;
+
+            Assert.AreEqual(new []{10, 0, 0, 0}, array.GetRow(0));
+            Assert.AreEqual(new []{0, 0, 0, 0}, array.GetRow(1));
+            Assert.Throws<IndexOutOfRangeException>(() => array.GetRow(-1));
+            Assert.Throws<IndexOutOfRangeException>(() => array.GetRow(2));
+        }
+
+        [Test]
+        public void ConversionTest() {
+            int[,] array = new int[2, 4];
+            array[0, 0] = 10;
+            array[1, 1] = 20;
+
+            int[][] converted = array.Jagged();
+            Assert.AreEqual(2, converted.Length);
+            Assert.AreEqual(4, converted[0].Length);
+            Assert.AreEqual(10, converted[0][0]);
+            Assert.AreEqual(20, converted[1][1]);
+            Assert.AreEqual(array, converted.UnJagged());
+
+            
+            int[][] jagged = new int[2][];
+            jagged[0] = new int[3];
+            jagged[1] = new int[4];
+
+            Assert.Throws<IndexOutOfRangeException>(() => jagged.UnJagged());
+            Assert.AreEqual(2, jagged.UnJagged(true).Rows());
+            Assert.AreEqual(4, jagged.UnJagged(true).Columns());
+        }
+    }
+}
---
+++ 2021/UtilExtensions/ArrayExtensions.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace UtilExtensions {
+    public static class ArrayExtensions {
+        public static int Rows<T>(this T[,] arr) => arr.GetLength(0);
+        public static int Columns<T>(this T[,] arr) => arr.GetLength(1);
+        
+        public static bool TryGet<T>(this T[,] arr, int row, int col, out T value) {
+            if (row < 0 || col < 0 || row >= arr.GetLength(0) || col >= arr.GetLength(1)) {
+                value = default;
+                return false;
+            }
+
+            value = arr[row, col];
+            return true;
+        }
+
+        public static T[] GetColumn<T>(this T[,] arr, int col) {
+            int length = arr.GetLength(0);
+            
+            var result = new T[length]; 
+            for (int i = 0; i < length; i++) {
+                result[i] = arr[i, col];
+            }
+
+            return result;
+        }
+
+        public static T[] GetRow<T>(this T[,] arr, int row) {
+            int length = arr.GetLength(1);
+            
+            var result = new T[length]; 
+            for (int i = 0; i < length; i++) {
+                result[i] = arr[row, i];
+            }
+
+            return result;
+        }
+
+        public static T[][] Jagged<T>(this T[,] arr) {
+            int cols = arr.GetLength(0);
+            var result = new T[cols][];
+            for (int i = 0; i < cols; i++) {
+                result[i] = arr.GetRow(i);
+            }
+
+            return result;
+        }
+        
+        public static T[,] UnJagged<T>(this T[][] arr, bool expand = false) {
+            int rows = arr.Length;
+            int cols = arr[0].Length;
+            if (expand) {
+                cols = arr.Select(row => row.Length).Max();
+            }
+
+            var result = new T[rows, cols];
+            for (int i = 0; i < rows; i++) {
+                if (!expand && arr[i].Length != cols) {
+                    throw new IndexOutOfRangeException("Jagged array cannot be converted without expanding.");
+                }
+                for (int j = 0; j < cols && j < arr[i].Length; j++) {
+                    result[i, j] = arr[i][j];
+                }
+            }
+
+            return result;
+        }
+
+        public static string PrettyString<T>(this T[,] arr, string delimiter = " ") {
+            var builder = new StringBuilder();
+            for (int i = 0; i < arr.Rows(); i++) {
+                builder.AppendLine(string.Join(delimiter, arr.GetRow(i)));
+            }
+
+            return builder.ToString();
+        }
+    }
+}
