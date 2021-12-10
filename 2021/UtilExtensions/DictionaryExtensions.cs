268bddcefa894b9e6101b60d2f9c327da4c9a403
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2021-12-09 23:36:36 -0800

    Add DictionaryExtensions

---
+++ 2021/UtilExtensions/DictionaryExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UtilExtensions {
+    public static class DictionaryExtensions {
+        public static TV GetOrCreate<TK, TV>(this IDictionary<TK, TV> dict, TK key, TV @default = default) {
+            if (!dict.TryGetValue(key, out TV value)) {
+                value = @default;
+                dict[key] = value;
+            }
+            
+            return value;
+        }
+        
+        public static TK GetByValue<TK, TV>(this IDictionary<TK, TV> dict, TV get) {
+            foreach ((TK key, TV value) in dict) {
+                if (EqualityComparer<TV>.Default.Equals(get, value)) {
+                    return key;
+                }
+            }
+
+            return default;
+        }
+        
+        public static bool OneToOne<TK, TV>(this IDictionary<TK ,TV> dict) {
+            var values = new HashSet<TV>();
+            foreach ((TK _, TV value) in dict) {
+                if (values.Contains(value)) {
+                    return false;
+                }
+                
+                values.Add(value);
+            }
+
+            return true;
+        }
+
+        public static Dictionary<TV, TK> Reverse<TK, TV>(this IDictionary<TK, TV> dict) {
+            if (!dict.OneToOne()) {
+                throw new ArgumentException("Dictionary must be one-to-one to reverse.");
+            }
+
+            var result = new Dictionary<TV, TK>();
+            foreach ((TK key, TV value) in dict) {
+                result[value] = key;
+            }
+            
+            return result;
+        }
+
+        public static IEnumerable<KeyValuePair<TK, TV>> Sort<TK, TV>(this IDictionary<TK, TV> dict) {
+            return dict.OrderBy(pair => pair.Key);
+        }
+        
+        public static IEnumerable<KeyValuePair<TK, TV>> SortByValue<TK, TV>(this IDictionary<TK, TV> dict) {
+            return dict.OrderBy(pair => pair.Value);
+        }
+    }
+}
