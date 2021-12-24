69caa650c0f594b0fd1908a196a734b49b0b0340
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2021-12-24 02:02:34 -0800

    Day 24 - Alt

--- 2021/Day24/Program.cs
+++ 2021/Day24/Program.cs
@@ -1,69 +1,84 @@
-﻿using UtilExtensions;
+﻿using System.Text.RegularExpressions;
+using UtilExtensions;
 
 namespace Day24 {
     // https://adventofcode.com/
     public static class Program {
         public static void Main() {
             string file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
-            var input = File.ReadAllLines(file).Select(row => {
-                if (row.StartsWith('#')) return default;
+            string[] blocks = File.ReadAllText(file).Split("inp w").Skip(1).ToArray();
 
-                string[] split = row.Split(" ");
-                Instruction.Operation op;
-                switch (split[0]) {
-                    case "inp":
-                        op = Instruction.Operation.Input;
-                        break;
-                    case "add":
-                        op = Instruction.Operation.Add;
-                        break;
-                    case "mul":
-                        op = Instruction.Operation.Multiply;
-                        break;
-                    case "div":
-                        op = Instruction.Operation.Divide;
-                        break;
-                    case "mod":
-                        op = Instruction.Operation.Mod;
-                        break;
-                    case "eql":
-                        op = Instruction.Operation.Equals;
-                        break;
-                    case "set":
-                        op = Instruction.Operation.Set;
-                        break;
-                    case "print":
-                        op = Instruction.Operation.Print;
-                        break;
-                    default:
-                        throw new ArgumentException($"invalid operation: {split[0]}");
+            var rules = new (int, int)[14];
+            var stack = new Stack<(int, int)>();
+            for (int i = 0; i < 14; i++) {
+                string step = blocks[i];
+                if (step.Contains("div z 26")) { // Reduce Z step
+                    Match match = Regex.Match(step, "add x (-?\\d+)");
+                    int addX = int.Parse(match.Groups[1].Value);
+                    (int index, int addY) = stack.Pop();
+
+                    int offset = addY + addX;
+                    rules[index] = (i, offset);
+                    rules[i] = (index, -offset);
+                } else { // Increase Z step
+                    Match match = Regex.Match(step, "add y w\r\nadd y (-?\\d+)");
+                    int addY = int.Parse(match.Groups[1].Value);
+                    stack.Push((i, addY));
                 }
+            }
 
-                string a = split[1];
-                string b = null;
-                if (split.Length > 2) {
-                    b = split[2];
-                }
-
-                return new Instruction(op, a, b);
-            }).Where(i => i != default).ToArray();
-
-            //Cli(input);
-
-            /*** Solution ***
-             * w5 = w4
-             * w6 = w3 + 4
-             * w7 = w2 + 8
-             * w10 = w9 + 6
-             * w11 = w8 + 5
-             * w12 = w1 - 6
-             * w14 = w13 - 4
-             ***************/
-            Console.WriteLine(Part1(input, 91599994399395L));
-            Console.WriteLine(Part2(input, 71111591176151L));
+            Console.WriteLine(Part1(rules));
+            Console.WriteLine(Part2(rules));
         }
 
-        private static void Cli(Instruction?[] instructions) {
+        private static long Part1((int, int)[] rules) {
+            long number = 0;
+            for (int i = 0; i < 14; i++) {
+                number *= 10;
+                number += Math.Min(9 - rules[i].Item2, 9);
+            }
+
+            return number;
+        }
+
+        private static long Part2((int, int)[] rules) {
+            long number = 0;
+            for (int i = 0; i < 14; i++) {
+                number *= 10;
+                number += Math.Max(1 - rules[i].Item2, 1);
+            }
+
+            return number;
+        }
+
+        // Interpreter
+        private static void Cli() {
+            string file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+            Instruction[] instructions = File.ReadAllLines(file)
+                .Where(row => !row.StartsWith("#"))
+                .Select(row => {
+                    string[] split = row.Split(" ");
+                    Operation op = split[0] switch {
+                        "inp" => Operation.Input,
+                        "add" => Operation.Add,
+                        "mul" => Operation.Multiply,
+                        "div" => Operation.Divide,
+                        "mod" => Operation.Mod,
+                        "eql" => Operation.Equals,
+                        "set" => Operation.Set,
+                        "print" => Operation.Print,
+                        _ => throw new ArgumentException($"invalid operation: {split[0]}")
+                    };
+
+                    string a = split[1];
+                    string b = string.Empty;
+                    if (split.Length > 2) {
+                        b = split[2];
+                    }
+
+                    return new Instruction(op, a, b);
+                }).ToArray();
+
             while (true) {
                 string input = Console.ReadLine() ?? string.Empty;
                 if (string.IsNullOrEmpty(input)) continue;
@@ -88,96 +103,58 @@ namespace Day24 {
             }
         }
 
-        public record Instruction(Instruction.Operation Op, string A, string B) {
-            public enum Operation {
-                Input,
-                Add,
-                Multiply,
-                Divide,
-                Mod,
-                Equals,
-                Set,
-                Print,
-            }
+        private record Instruction(Operation Op, string A, string B);
 
-            public override string ToString() {
-                return $"{Op} {A} {B}";
-            }
+        private enum Operation {
+            Input,
+            Add,
+            Multiply,
+            Divide,
+            Mod,
+            Equals,
+            Set,
+            Print,
         }
 
-        private static DefaultDictionary<string, long> Execute(Instruction?[] instructions, long input) {
+        private static DefaultDictionary<string, long> Execute(Instruction[] instructions, long input) {
             int[] inputs = input.ToString().ToCharArray().Select(c => c - '0').ToArray();
             int i = 0;
             DefaultDictionary<string, long> variables = new();
-            foreach ((Instruction.Operation op, string? a, string? b) in instructions) {
+            foreach ((Operation op, string a, string b) in instructions) {
+                long bVal = long.TryParse(b, out long val) ? val : variables[b];
                 switch (op) {
-                    case Instruction.Operation.Input:
+                    case Operation.Input:
                         variables[a] = inputs[i];
                         i++;
                         break;
-                    case Instruction.Operation.Add:
-                        if (int.TryParse(b, out int addB)) {
-                            variables[a] += addB;
-                        } else {
-                            variables[a] += variables[b];
-                        }
+                    case Operation.Add:
+                        variables[a] += bVal;
                         break;
-                    case Instruction.Operation.Multiply:
-                        if (int.TryParse(b, out int mulB)) {
-                            variables[a] *= mulB;
-                        } else {
-                            variables[a] *= variables[b];
-                        }
+                    case Operation.Multiply:
+                        variables[a] *= bVal;
                         break;
-                    case Instruction.Operation.Divide:
-                        if (int.TryParse(b, out int divB)) {
-                            variables[a] /= divB;
-                        } else {
-                            variables[a] /= variables[b];
-                        }
+                    case Operation.Divide:
+                        variables[a] /= bVal;
                         break;
-                    case Instruction.Operation.Mod:
-                        if (int.TryParse(b, out int modB)) {
-                            variables[a] %= modB;
-                        } else {
-                            variables[a] %= variables[b];
-                        }
+                    case Operation.Mod:
+                        variables[a] %= bVal;
                         break;
-                    case Instruction.Operation.Equals:
-                        if (int.TryParse(b, out int eqB)) {
-                            variables[a] = variables[a] == eqB ? 1 : 0;
-                        } else {
-                            variables[a] = variables[a] == variables[b] ? 1 : 0;
-                        }
+                    case Operation.Equals:
+                        variables[a] = variables[a] == bVal ? 1 : 0;
                         break;
-                    case Instruction.Operation.Set:
-                        if (int.TryParse(b, out int setB)) {
-                            variables[a] = setB;
-                        } else {
-                            variables[a] = variables[b];
-                        }
+                    case Operation.Set:
+                        variables[a] = bVal;
                         break;
-                    case Instruction.Operation.Print:
-                        Console.WriteLine($"{a} is {variables[a]}");
+                    case Operation.Print:
+                        Console.WriteLine($"{a}: {variables[a]}");
                         break;
                     default:
-                        throw new ArgumentOutOfRangeException("invalid operation");
+                        throw new ArgumentException($"Invalid instruction: {op}");
                 }
             }
 
+            variables.Remove(string.Empty); // Useless placeholder
             return variables;
-        }
-
-        private static bool Part1(Instruction?[] instructions, long num) {
-            Console.WriteLine(num);
-            DefaultDictionary<string, long> vars = Execute(instructions, num);
-            return vars["z"] == 0;
-        }
-
-        private static bool Part2(Instruction?[] instructions, long num) {
-            Console.WriteLine(num);
-            DefaultDictionary<string, long> vars = Execute(instructions, num);
-            return vars["z"] == 0;
         }
     }
 }
