355902632c583b8ea5d2cc273733c1afdc36b42a
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2022-12-15 14:05:39 -0800

    Day 15

---
+++ 2022/Day15/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.Drawing;
+using UtilExtensions;
+
+namespace Day15;
+
+// https://adventofcode.com/
+public static class Program {
+    public static void Main() {
+        string file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+
+        List<(Point, Point)> data = new();
+        foreach (string line in File.ReadAllLines(file)) {
+            int[] args = line.Extract(@"x=(-?\d+), y=(-?\d+).+x=(-?\d+), y=(-?\d+)").Select(int.Parse).ToArray();
+            data.Add((new Point(args[0], args[1]), new Point(args[2], args[3])));
+        }
+
+        Console.WriteLine(Part1(data));
+        Console.WriteLine(Part2(data));
+    }
+
+    private static int Part1(IList<(Point, Point)> data, int target = 2000000) {
+        var intervals = new IntervalCollection();
+        var remove = new IntervalCollection();
+        foreach ((Point scanner, Point beacon) in data) {
+            if (beacon.Y == target) {
+                remove.Add(beacon.X, beacon.X);
+            }
+            if (scanner.Y == target) {
+                remove.Add(scanner.X, scanner.X);
+            }
+
+            int dX = Math.Abs(scanner.X - beacon.X);
+            int dY = Math.Abs(scanner.Y - beacon.Y);
+            int distance = dX + dY;
+
+            int delta = Math.Abs(scanner.Y - target);
+            if (delta > distance) {
+                continue;
+            }
+
+            int length = distance - delta;
+            intervals.Add(scanner.X - length, scanner.X + length);
+        }
+        
+        foreach (Interval interval in remove) {
+            intervals.Remove(interval);
+        }
+        intervals.Reduce();
+        return intervals.Select(interval => interval.Size).Sum();
+    }
+
+    private static long Part2(IList<(Point, Point)> data, int limit = 4000000) {
+        return Enumerable.Range(0, limit)
+            .AsParallel()
+            .Select(target => {
+                var intervals = new IntervalCollection();
+                foreach ((Point scanner, Point beacon) in data) {
+                    if (beacon.Y == target) {
+                        intervals.Add(beacon.X, beacon.X);
+                    }
+                    if (scanner.Y == target) {
+                        intervals.Add(scanner.X, scanner.X);
+                    }
+
+                    int dX = Math.Abs(scanner.X - beacon.X);
+                    int dY = Math.Abs(scanner.Y - beacon.Y);
+                    int distance = dX + dY;
+
+                    int delta = Math.Abs(scanner.Y - target);
+                    if (delta > distance) {
+                        continue;
+                    }
+
+                    int length = distance - delta;
+                    intervals.Add(scanner.X - length, scanner.X + length);
+                }
+
+                intervals.Reduce();
+                intervals.Clamp(0, limit);
+                int count = intervals.Select(interval => interval.Size).Sum();
+                if (count != limit + 1) {
+                    Interval first = intervals.First();
+                    if (intervals.Count == 1) {
+                        if (first.Start == 1) {
+                            return target;
+                        } if (first.End == limit - 1) {
+                            return limit * 4000000L + target;
+                        }
+                        throw new InvalidOperationException($"Invalid gap in range: {first}");
+                    }
+                    
+                    return (intervals.First().End + 1) * 4000000L + target;
+                }
+                return 0;
+            })
+            .Single(value => value > 0);
+    }
+}
---
+++ UtilExtensions/IntervalCollection.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UtilExtensions;
+
+public class IntervalCollection : ICollection<Interval> {
+    private SortedSet<Interval> intervals;
+
+    public IntervalCollection() {
+        intervals = new SortedSet<Interval>();
+    }
+
+    public void Reduce() {
+        var stack = new Stack<Interval>();
+        using SortedSet<Interval>.Enumerator enumerator = intervals.GetEnumerator();
+        if (!enumerator.MoveNext()) {
+            return;
+        }
+        stack.Push(enumerator.Current);
+        
+        while (enumerator.MoveNext()) {
+            Interval current = enumerator.Current;
+            Interval top = stack.Peek();
+
+            if (top.End + 1 == current.Start || current.End + 1 == top.Start) {
+                stack.Pop();
+                stack.Push(top with {End = current.End});
+            } else if (top.End.CompareTo(current.Start) < 0) {
+                stack.Push(current);
+            } else if (top.End.CompareTo(current.End) <= 0) {
+                stack.Pop();
+                stack.Push(top with {End = current.End});
+            }
+        }
+
+        intervals = new SortedSet<Interval>(stack);
+    }
+
+    public void Clamp(int min, int max) {
+        var result = new SortedSet<Interval>();
+        foreach (Interval interval in intervals) {
+            if (interval.End < min || interval.Start > max) {
+                continue;
+            }
+
+            int start = Math.Max(interval.Start, min);
+            int end = Math.Min(interval.End, max);
+            result.Add(new Interval(start, end));
+        }
+
+        intervals = result;
+    }
+    
+    public IEnumerator<Interval> GetEnumerator() => intervals.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public void Add(int start, int end) => Add(new Interval(start, end));
+    public void Add(Interval item) => intervals.Add(item);
+
+    public void Clear() => intervals.Clear();
+
+    public bool Contains(Interval item) => intervals.Contains(item);
+
+    public void CopyTo(Interval[] array, int arrayIndex) => intervals.CopyTo(array, arrayIndex);
+
+    public bool Remove(Interval remove) {
+        var result = new SortedSet<Interval>();
+        bool modified = false;
+        foreach (Interval interval in intervals) {
+            if (remove.Contains(interval)) {
+                // Interval:  |---|
+                // Remove:  |-------|
+                // Result:
+                modified = true;
+                continue;
+            }
+            if (!interval.Overlaps(remove)) {
+                // Interval:      |-----|
+                // Remove:  |---|
+                // Result:        |-----|
+                result.Add(interval);
+                continue;
+            }
+            
+            if (remove.Start.CompareTo(interval.Start) <= 0) {
+                // Interval:   |-----|
+                // Remove:  |----|
+                // Result:        |--|
+                result.Add(interval with {Start = remove.End + 1});
+            } else if (remove.End.CompareTo(interval.End) >= 0) {
+                // Interval: |-----|
+                // Remove:       |----|
+                // Result:   |--|
+                result.Add(interval with {End = remove.Start - 1});
+            } else {
+                // Interval: |---------|
+                // Remove:      |--|
+                // Result:   |-|    |--|
+                result.Add(interval with {End = remove.Start - 1});
+                result.Add(interval with {Start = remove.End + 1});
+            }
+        }
+
+        intervals = result;
+        return modified;
+    }
+
+    public int Count => intervals.Count;
+    public bool IsReadOnly => false;
+}
+
+public record struct Interval(int Start, int End) : IComparable<Interval> {
+    public int Size => End - Start + 1;
+    
+    public int CompareTo(Interval other) {
+        int startComparison = Start.CompareTo(other.Start);
+        return startComparison != 0 ? startComparison : End.CompareTo(other.End);
+    }
+
+    public bool Contains(Interval other) {
+        return Start.CompareTo(other.Start) <= 0 && End.CompareTo(other.End) >= 0;
+    }
+
+    public bool Overlaps(Interval other) {
+        return Start.CompareTo(other.End) <= 0 && End.CompareTo(other.Start) >= 0;
+    }
+}
