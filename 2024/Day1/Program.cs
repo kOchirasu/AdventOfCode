43f7753cc407609142a315fcf4c3363445b31b6d
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2024-12-02 12:53:39 -0800

    2024, Day 1 & 2

---
+++ 2024/Day1/Program.cs
@@ -0,0 +1,47 @@
+﻿using UtilExtensions;
+
+namespace Day1;
+
+// https://adventofcode.com/
+public static class Program {
+    public static void Main() {
+        string file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+
+        var left = new List<int>();
+        var right = new List<int>();
+        foreach (string row in File.ReadAllLines(file)) {
+            string[] nums = row.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            left.Add(int.Parse(nums[0]));
+            right.Add(int.Parse(nums[1]));
+        }
+
+        Console.WriteLine(Part1(left, right));
+        Console.WriteLine(Part2(left, right));
+    }
+
+    private static int Part1(List<int> left, List<int> right) {
+        left.Sort();
+        right.Sort();
+
+        int distance = 0;
+        for (int i = 0; i < left.Count; i++) {
+            distance += Math.Abs(left[i] - right[i]);
+        }
+
+        return distance;
+    }
+
+    private static int Part2(IList<int> left, IList<int> right) {
+        var counts = new DefaultDictionary<int, int>();
+        foreach (int n in right) {
+            counts[n]++;
+        }
+
+        int score = 0;
+        foreach (int n in left) {
+            score += n * counts[n];
+        }
+
+        return score;
+    }
+}
---
+++ 2024/Day2/Program.cs
@@ -0,0 +1,63 @@
+﻿using UtilExtensions;
+
+namespace Day2;
+
+// https://adventofcode.com/
+public static class Program {
+    public static void Main() {
+        string file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+
+        var input = new List<List<int>>();
+        foreach (string row in File.ReadAllLines(file)) {
+            List<int> nums = row.Split(" ").Select(int.Parse).ToList();
+            input.Add(nums);
+        }
+
+        Console.WriteLine(Part1(input));
+        Console.WriteLine(Part2(input));
+    }
+
+    private static bool IsSafe(IList<int> test) {
+        int delta = test[1] - test[0];
+        if (Math.Abs(delta) is < 1 or > 3) {
+            return false;
+        }
+
+        bool direction = delta > 0;
+        for (int i = 1; i < test.Count; i++) {
+            delta = test[i] - test[i - 1];
+            if (direction != (delta > 0) || Math.Abs(delta) is < 1 or > 3) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static int Part1(IList<List<int>> input) {
+        int safe = 0;
+        foreach (List<int> test in input) {
+            if (IsSafe(test)) {
+                safe++;
+            }
+        }
+
+        return safe;
+    }
+
+    private static int Part2(IList<List<int>> input) {
+        int safe = 0;
+        foreach (List<int> test in input) {
+            for (int i = 0; i < test.Count; i++) {
+                var copy = new List<int>(test);
+                copy.RemoveAt(i);
+                if (IsSafe(copy)) {
+                    safe++;
+                    break;
+                }
+            }
+        }
+
+        return safe;
+    }
+}
