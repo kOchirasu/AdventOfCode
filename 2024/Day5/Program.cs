f30c8fdd800f176f5136e9c46f0e712a76543c75
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2024-12-04 22:55:05 -0800

    Day 5

---
+++ 2024/Day5/Program.cs
@@ -0,0 +1,62 @@
+﻿using QuikGraph;
+using QuikGraph.Algorithms;
+using UtilExtensions;
+
+namespace Day5;
+
+// https://adventofcode.com/
+public static class Program {
+    public static void Main() {
+        string file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+
+        string[] groups = File.ReadAllText(file).Groups(trim: true);
+
+        List<(int, int)> rules = groups[0].ExtractAll(@"(\d+)\|(\d+)")
+            .Select(r => (int.Parse(r[0]), int.Parse(r[1])))
+            .ToList();
+
+        List<int[]> updates = groups[1].StringList()
+            .Select(str => str.Split(",").Select(int.Parse).ToArray())
+            .ToList();
+
+        Console.WriteLine(Part1(rules, updates));
+        Console.WriteLine(Part2(rules, updates));
+    }
+
+    private static int[] TopologicalSort(List<(int, int)> rules, int[] update) {
+        HashSet<int> set = update.ToHashSet();
+
+        var graph = new AdjacencyGraph<int, TaggedEdge<int, int>>();
+        foreach ((int x, int y) in rules) {
+            if (set.Contains(x) && set.Contains(y)) {
+                graph.AddVerticesAndEdge(new TaggedEdge<int, int>(x, y, 1));
+            }
+        }
+
+        return graph.TopologicalSort().ToArray();
+    }
+
+    private static int Part1(List<(int, int)> rules, List<int[]> updates) {
+        int sum = 0;
+        foreach (int[] update in updates) {
+            int[] sorted = TopologicalSort(rules, update);
+            if (update.SequenceEqual(sorted)) {
+                sum += sorted[sorted.Length / 2];
+            }
+        }
+
+        return sum;
+    }
+
+    private static int Part2(List<(int, int)> rules, List<int[]> updates) {
+        int sum = 0;
+        foreach (int[] update in updates) {
+            int[] sorted = TopologicalSort(rules, update);
+            if (!update.SequenceEqual(sorted)) {
+                sum += sorted[sorted.Length / 2];
+            }
+        }
+
+        return sum;
+    }
+}
