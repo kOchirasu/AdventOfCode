4798e4143e6994458e80c212bb604ce543bc781c
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2024-12-22 23:08:11 -0800

    Day 24 Template

---
+++ 2024/Day24/Program.cs
@@ -0,0 +1,33 @@
+﻿using UtilExtensions;
+using static UtilExtensions.ArrayExtensions;
+
+namespace Day24;
+
+// https://adventofcode.com/
+public static class Program {
+    public static void Main() {
+        string file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+
+        // var input = File.ReadAllLines(file).CharMatrix();
+        // var input = File.ReadAllText(file).IntList();
+        string input = File.ReadAllText(file);
+        foreach (string row in File.ReadAllLines(file)) {
+
+        }
+
+        // Console.WriteLine(Part1());
+        // Console.WriteLine(Part2());
+    }
+
+    private static int Part1() {
+        int result = 0;
+
+        return result;
+    }
+
+    private static int Part2() {
+        int result = 0;
+
+        return result;
+    }
+}
